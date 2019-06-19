297bcf68d5ecbe12a24951a700dee3cecab400c1
SafiaCoding <safia.coding@gmail.com>
2019-06-19 16:08:42 +0200

    Action of turning cards

--- ConcentreGame/ViewController.swift
+++ ConcentreGame/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    var turnCount: Int = 0 {
+        didSet {
+            countLabel.text = "CardClicks: \(turnCount)"
+        }
+    }
+    
+    var cardContent: [String] = ["😇", "🙌🏻","😇","🙌🏻"]
+    
+    @IBAction func clickCard(_ sender: UIButton) {
+        turnCount += 1
+        
+        if let cardIndex = cardButtons.firstIndex(of: sender) {
+            turnCard(emoji: cardContent[cardIndex], button: sender)
+        } else {
+            print("error in choising cards")
+        }
+            
     }
 
-
+    func turnCard(emoji: String, button: UIButton){
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+    
 }
 
